4b207c02ac40225bbed208f1ecc9fe9c33313122
bzhu3216 <tozhubin@163.com>
2019-07-12 15:13:27 +0800

    werewolf
    玩儿翁

--- OnlineTest/src/com/qh/test/action/UserAction.java
+++ OnlineTest/src/com/qh/test/action/UserAction.java
@@ -27,6 +27,17 @@ import com.qh.test.util.StringUtil;
 import net.sf.json.JSONObject;
 
 public class UserAction extends ActionSupport implements ServletRequestAware {
+	
+	////////////////////////////////
+	// 上传文件存储目录
+    private static final String UPLOAD_DIRECTORY = "upload";
+ 
+    // 上传配置
+    private static final int MEMORY_THRESHOLD   = 1024 * 1024 * 3;  // 3MB
+    private static final int MAX_FILE_SIZE      = 1024 * 1024 * 40; // 40MB
+    private static final int MAX_REQUEST_SIZE   = 1024 * 1024 * 50; // 50MB
+	///////////////////////////////////
+	
 
 	User user;
 	User selUser;
